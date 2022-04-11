4b7ba4da3cfe58ae737a35d94fafe277d1d0b2e8
Derriel Collins <derrielcollins96@gmail.com>
2022-04-11 16:12:58 -0500

    connected front-end form to backend form, text message functionality now works

--- craftco-app/src/components/Consult/Consult.js
+++ craftco-app/src/components/Consult/Consult.js
@@ -4,15 +4,56 @@ import React, { useState } from 'react';
 const Consult = () => {
 	//Need to replace this with full form
 	const [textArea, setTextArea] = useState("");
+	const [firstName , setFirstName] = useState("");
+	const [lastName , setLastName] = useState("");
+	const [phoneNumber , setPhoneNumber] = useState("");
+	const [address , setAddress] = useState("");
+
+	const data = {			
+				firstName: firstName,
+				lastName: lastName,
+				phoneNumber: phoneNumber,
+				address: address,
+				textArea: textArea,
+			};
 
 	const handleSubmit = (e) => {
-		alert('Your Message has been sent!')
-		e.preventDefault();
+		fetch('http://localhost:3001/consult',{
+			method: 'post',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				data: data,
+			})
+		})
+		.then(response => {
+			response.json();
+			alert('Your Message has been sent!');
+		})
+
+	};
+
+	const handleTextAreaChange = (e) => {
+		setTextArea(e.target.value);
+
+	}
+	const handleFirstNameChange = (e) => {
+		setFirstName(e.target.value);
+		}
+
+	const handleLastNameChange = (e) => {
+		setLastName(e.target.value);
 	}
 
-	const handleChange = (e) => {
-		setTextArea(e.target.value);
+	const handlePhoneNumberChange = (e) => {
+		setPhoneNumber(e.target.value);
 	}
+
+	const handleAddressChange = (e) => {
+		setAddress(e.target.value);
+	}
+
+
+
 
 	return (
 	<div>
@@ -20,8 +61,24 @@ const Consult = () => {
         <h1 className="glow neonText background">Contact Us For a Consultation!</h1>
         <form onSubmit={handleSubmit}>
         	<label>Enter In Your Contact Info and a brief description of your desired Pergola.</label>
-        	<div className="textarea"><textarea value={textArea} onChange={handleChange} /></div>
-        	<div><input className="button" type="submit" value="Shoot Us a Text!" /></div>	
+        		<div className="">
+        			<label>First Name: </label>
+        			<input onChange={handleFirstNameChange} className="textbox" type="textbox" placeholder="First Name" value={firstName} />
+        			<label> Last Name: </label>
+        			<input onChange={handleLastNameChange} className="textbox" type="textbox" placeholder="Last Name" value={lastName} />
+        		</div>
+	        	<div className="">
+	        		<label> Phone Number: </label>
+	        		<input onChange={handlePhoneNumberChange} className="textbox" type="textbox" placeholder="Phone Number" value={phoneNumber} />
+	        	</div>
+	        	<div className="">
+	        		<label> Address: </label>
+	        		<textarea onChange={handleAddressChange} className="textbox" type="textarea" rows="1" cols="20" placeholder="Address" value={address} />
+	        	</div>
+	        	<div>
+	        		<textarea value={textArea} onChange={handleTextAreaChange} rows="10" cols="60"/>
+	        	</div>
+	        	<div><input className="button" type="submit" value="Shoot Us a Text!" /></div>	
         </form>
         <div className="contact-card">        	
           <h1 className="f4">Owner & Operator: Chris Rodgers</h1>
@@ -33,7 +90,7 @@ const Consult = () => {
 					    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					    proident, sunt in culpa qui officia deserunt mollit anim id est laborum. - CHRIS RODGERS
 					</p>
-          <p> Located in the DFW Area!</p>
+          <address> Located in the DFW Area!</address>
         </div>
       </div>
 	</div>
